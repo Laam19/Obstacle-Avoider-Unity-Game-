078a7b8ebc3c79c525c6baaaba8f83a2861387e4
Laam19 <akib19@student.sust.edu>
2021-04-21 12:56:29 +0600

    fixed double jumping bug

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody playerRB;
+    private Rigidbody playerRB;
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool isOnGround = true;
     // Start is called before the first frame update
     void Start()
     {
         playerRB=GetComponent<Rigidbody>();
-        
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space)&& isOnGround)
         {
-            playerRB.AddForce(Vector3.up * 10,ForceMode.Impulse);
+            playerRB.AddForce(Vector3.up * jumpForce,ForceMode.Impulse);
+            isOnGround = false;
         }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
