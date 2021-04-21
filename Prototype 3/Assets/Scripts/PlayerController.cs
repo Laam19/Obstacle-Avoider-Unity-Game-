ddd95a3a8401bee2cfbb6f7f3f0f1354c9cd8c25
Laam19 <akib19@student.sust.edu>
2021-04-21 13:50:17 +0600

    Spawn obstacle

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -8,7 +8,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
-    // Start is called before the first frame update
+
     void Start()
     {
         playerRB=GetComponent<Rigidbody>();
